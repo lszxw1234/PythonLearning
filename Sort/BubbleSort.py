bf5adcb686c0f8ac2bb0877a5d6aab338cb81634
unknown <>
2016-08-24 21:54:54 +0800

    add insert sort & select sort

--- Sort/BubbleSort.py
+++ Sort/BubbleSort.py
@@ -8,22 +8,35 @@ def bubble_sort(list):
             if list[j-1] > list[j]:
                 #replace these two number.
                 list[j-1],list[j] = list[j],list[j-1]
-
     return list
 
-def selection_sort(list):
-    n=len(list)
-   for i in range (0,n):
-       min = i
-       for j in range(i+1,n):
-           if list[j]<list[min]:
-               min=j
-               list[min],list[i]=list[i],list[min]
+def selection_Sort(list):
+    n = len(list)
+    for i in range(0,n):
+        m = i
+        for j in range(i + 1, n):
+            if list[m]>list[j]:
+                m = j
+        list[m],list[i] = list[i], list[m]
     return list
+
+def insertion_sort(list):
+    n = len(list)
+    for i in range(1,n):
+        temp = list[i]
+        j = i
+        while j > 0 and temp < list[j-1]:
+            list[j] = list[j-1]
+            j -= 1
+        list[j] = temp
+    return list
+
 
 
 
 list1 = [3,1,2,5,6,9,4]
 list2 = copy.copy(list1)
+list3 = copy.copy(list1)
 print(bubble_sort(list1))
-print (selection_Sort(list2))
+print(selection_Sort(list2))
+print insertion_sort(list3)
